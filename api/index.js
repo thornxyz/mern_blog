a1a5a5c95696a1e267dc6d9d77eb5ce203f1b776
Thorn <subhrajyoti.ghosh@outlook.com>
2024-01-07 14:08:07 +0530

    fixed multer

--- api/index.js
+++ api/index.js
@@ -11,14 +11,11 @@ const Post = require('./models/Post')
 
 const multer = require('multer');
 const cloudinary = require('./cloudinary.js');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'mern-blog',
-        allowed_formats: ['jpg', 'jpeg', 'png', 'gif'],
-    },
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
 });
 
 const uploadMiddleware = multer({ storage: storage });
@@ -33,12 +30,18 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect(process.env.MONGO_URI);
 
-mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB');
-});
-mongoose.connection.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-});
+function setupMongoDBConnection() {
+    mongoose.connection.on('connected', () => {
+        console.log('Connected to MongoDB');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.error('MongoDB connection error:', err);
+    });
+}
+
+setupMongoDBConnection();
+
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
@@ -57,13 +60,10 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const userDoc = await User.findOne({ username });
-
     if (!userDoc) {
         return res.status(400).json('User not found');
     }
-
     const passOk = bcrypt.compareSync(password, userDoc.password);
-
     if (passOk) {
         jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
@@ -89,7 +89,7 @@ app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMiddleware.single('file'), async (req, res) => {    
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
     const { token } = req.cookies;
     jwt.verify(token, secret, {}, async (err, info) => {
         if (err) throw err;
