96718587d05f55266449b3644f96bfe6636e19d8
Thorn <subhrajyoti.ghosh@outlook.com>
2024-01-09 19:30:36 +0530

    dependencies again

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -23,7 +23,7 @@ export default function PostPage() {
       <h1>{postInfo.title}</h1>
       <time>{format(new Date(postInfo.createdAt), "MMM d, yyyy HH:mm")}</time>
       <div className="author">by @{postInfo.author.username}</div>
-      {userInfo && userInfo.id === postInfo.author?.id && (
+      {userInfo && postInfo.author && userInfo.id === postInfo.author._id &&(
         <div className="edit-row">
           <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
